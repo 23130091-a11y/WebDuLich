e8b82a8192c41f295db8f38add26e543b26d54aa
itlord23 <nguyenthihonghanh5098@gmail.com>
2026-01-05 15:15:44 +0700

    Sửa db models, dựng admin, sửa lại truy vấn các thuộc tính trong views.py, cập nhật cách tính điểm: tổng điểm của nơi đó sẽ bằng điểm review 50% (rating) + sentiment 30% () + popularity 20% (yêu thích và lượt xem)

--- travel/urls.py
+++ travel/urls.py
@@ -16,20 +16,28 @@ urlpatterns = [
 
     # Thêm route cho destination detail
     # Sửa lại chỗ này
-    path('destination/<slug:slug>/', views.destination_detail, name='destination_detail'),
+    path('destination/<int:destination_id>/', views.destination_detail, name='destination_detail'),
     path('destinations/', views.destination_list, name='destination_list'),
 
     # API endpoints
     path('api/search/', views.api_search, name='api_search'),
+    path('api/provinces/', views.api_search_provinces, name='api_search_provinces'),
+    path('api/nearby/', views.api_nearby_places, name='api_nearby_places'),
     path('api/search-history/', views.api_search_history, name='api_search_history'),
     path('api/search-history/delete/', views.api_delete_search_history, name='api_delete_search_history'),
 
     # Review APIs (User-Generated Content)
+    path('api/review/', views.api_submit_review, name='api_submit_review'),
     path('api/review/vote/', views.api_vote_review, name='api_vote_review'),
     path('api/review/report/', views.api_report_review, name='api_report_review'),
 
     #profile account
     path('accountProfile/', views.account_profile, name='profile'),
     path("api/profile/", views.api_profile, name="api_profile"),
+
+    # Url danh sách yêu thích
+    path('favorites/', views.favorite_list, name='favorite_list'),
+    # Url yêu thích destination (id)
+    path('favorites/toggle/<int:destination_id>/', views.toggle_favorite, name='toggle_favorite'),
 
 ]
