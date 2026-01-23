463f6c007ae290c2ce8558718c82074987452581
HanaLeenaa <23130088@st.hcmuaf.edu.vn>
2026-01-23 14:03:09 +0700

    Update phan dat tour gom: dien form, tt, duyet roi gui email thong bao, them tag cho khach da hoan thanh tour khi review, chỉnh hien thi gia tour,...

--- myproject/settings.py
+++ myproject/settings.py
@@ -150,3 +150,13 @@ CACHE_TTL = {
 }
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'travel/static')]
+
+# gui email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# neu loi: EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
